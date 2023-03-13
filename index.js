3f89d9024dfc6fcfe4a3c1a297495c72434399a5
mohamedabdimohammud <mmohammud@outlook.com>
2023-03-13 15:09:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+// Select the dodger element
+const dodger = document.querySelector("#dodger");
+
+// Move the dodger left or right based on arrow key input
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    const leftPosition = parseInt(dodger.style.left);
+    dodger.style.left = `${leftPosition - 5}px`;
+  } else if (event.key === "ArrowRight") {
+    const leftPosition = parseInt(dodger.style.left);
+    dodger.style.left = `${leftPosition + 5}px`;
+  }
+});
